d6b98cf0eaa53fc4ae37801629b9235366818d8e
JennyShalai <jenny.shalai@gmail.com>
2016-10-10 16:20:30 -0400

    string comresstion - done

---
+++ task-12-string-compression.playground/Contents.swift
@@ -0,0 +1,37 @@
+// aabcccccaaa -> a2b1c5a3 if "compressed" string >= original string, return original string
+
+func compressString(str: String) -> String {
+    
+    var result: String = ""
+    var char: String = String(str[str.startIndex])
+    var counter: Int = 0
+    
+    for i in 0..<str.characters.count {
+        
+        let currentChar = String(str[str.index(str.startIndex, offsetBy: i)])
+        
+        if currentChar == char {
+            counter += 1
+        } else {
+            result += "\(char)\(counter)"
+            char = currentChar
+            counter = 1
+        }
+    }
+    
+    if result.characters.count >= str.characters.count {
+        return str
+    }
+    
+    return result
+}
+
+
+let str1 = "eeeeerrreeeeeewwwqqwd"
+print(compressString(str: str1))
+// e5r3e6w3q2w1
+
+
+let str2 = "ert"
+print(compressString(str: str2))
+// ert
