1202e3b99021bb1cf1a12111df3c844a05817ebe
JennyShalai <jenny.shalai@gmail.com>
2016-10-10 13:41:33 -0400

    add printing out non-unique chars

--- task-9-string-is-unique-chars.playground/Contents.swift
+++ task-9-string-is-unique-chars.playground/Contents.swift
@@ -1,4 +1,4 @@
-// Implement an aalgorithm to determine if a string has all unique characters.
+// Implement an aalgorithm to determine if a string has all unique characters. And print non-unique chars.
 
 
 func isUniqueCharsString(str: String) -> Bool {
@@ -11,12 +11,22 @@ func isUniqueCharsString(str: String) -> Bool {
         let currentChar = String(str[str.index(str.startIndex, offsetBy: i)])
         
         // add char in dictionary and if char already there - return false
+        // dict[currentChar] == nil ? (dict[currentChar] = 1) : (result = false)
         if dict[currentChar] == nil {
             dict[currentChar] = 1
         } else {
             result = false
+            dict[currentChar] = dict[currentChar]! + 1
         }
     
+    }
+    
+    for (key, val) in dict {
+        // if char seen more than 1 time - print it
+        if Int(val) > 1 {
+            print("\(key) \(val)")
+        }
+        
     }
     
     return result
@@ -24,8 +34,10 @@ func isUniqueCharsString(str: String) -> Bool {
 }
 
 
-let strFalse = "qwertyuiop[]asdfghjkl;'zxcvbnNmMQq"
+let strFalse = "qwertyuiop[]asdfghjkl;'zzzzxcvbnNmMQq"
 print(isUniqueCharsString(str: strFalse))
+// q 2
+// z 4
 // false
 
 
