48ad57e878cf09b079436611946360bbf8ffeaa7
JennyShalai <jenny.shalai@gmail.com>
2016-10-14 09:29:46 -0400

    check the correct brackets position

---
+++ task-15-brackets-validation.playground/Contents.swift
@@ -0,0 +1,35 @@
+// check the correct brackets position 
+// {({[]})()} -> true
+// {(})() -> false
+
+func bracketsValidation(str: String) -> Bool {
+
+    var array: [String] = []
+
+    for char in str.characters {
+        // when find open bracket - add it to array
+        if char == "{" || char == "[" || char == "(" {
+            array.append(String(char))
+        // when find close bracket and it had pair - delet it's pair from array
+        } else if char == "}" && array.count > 0 && array.last! == "{" {
+            array.removeLast()
+        } else if char == "]" && array.count > 0 && array.last! == "[" {
+            array.removeLast()
+        } else if char == ")" && array.count > 0 && array.last! == "(" {
+            array.removeLast()
+        } else {
+            return false
+        }
+    }
+    
+    if array.count == 0 {
+        return true
+    }
+    
+    return false
+}
+
+print(bracketsValidation(str: "()"))        // true
+print(bracketsValidation(str: "()("))       // false
+print(bracketsValidation(str: "[(){}]"))    // true
+print(bracketsValidation(str: "([)]"))      // false
