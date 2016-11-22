8bc26a21646c15d40c8d9bf34571d9348de679fc
JennyShalai <jenny.shalai@gmail.com>
2016-11-22 17:04:17 -0500

    array of positive ints, are there 2 elements sum = 50

--- task-27-sum==base-in-array.playground/Contents.swift
+++ task-27-sum==base-in-array.playground/Contents.swift
@@ -1,6 +1,9 @@
 //are there 2 elements in array of ints, sum of which == base?
 
-////  O(n2)
+///////////////////////////////////////////////////
+////////////////////   O(n2)   ////////////////////
+///////////////////////////////////////////////////
+
 func isElementsSumAsBase_N2(array: [Int], base: Int) -> Bool {
 
     if array.count < 2 {
@@ -21,10 +24,11 @@ func isElementsSumAsBase_N2(array: [Int], base: Int) -> Bool {
     return false
 }
 
-print(isElementsSumAsBase_N2(array: [2,4,5,3,9], base: 8))  // true
-print(isElementsSumAsBase_N2(array: [2,4,5,3,9], base: 10)) // false
 
-////  O(n)
+///////////////////////////////////////////////////
+////////////////////   O(n)   /////////////////////
+///////////////////////////////////////////////////
+
 func isElementsSumAsBase_N(array: [Int], base: Int) -> Bool {
     
     if array.count < 2 {
@@ -47,5 +51,40 @@ func isElementsSumAsBase_N(array: [Int], base: Int) -> Bool {
     return false
 }
 
+//////////////////////////////////////////////////////////////////////
+
+print(isElementsSumAsBase_N2(array: [2,4,5,3,9], base: 8))  // true
+print(isElementsSumAsBase_N2(array: [2,4,5,3,9], base: 10)) // false
+
 print(isElementsSumAsBase_N(array: [2,4,5,3,9], base: 8))  // true
 print(isElementsSumAsBase_N(array: [2,4,5,3,9], base: 10)) // false
+
+//////////////////////////////////////////////////////////////////////
+
+
+// given array of positive ints, are there 2 elements sum = 100?
+
+func isTowElemsHandred(array: [Int]) -> Bool {
+    
+    if array.count < 2 {
+        return false
+    }
+    
+    var dict: [Int: Int] = [:]
+    
+    for elem in array {
+        if elem < 100 {
+            if dict[elem] == nil {
+                dict[100-elem] = 100-elem
+            } else {
+                return true
+            }
+        }
+        
+    }
+    
+    return false
+}
+
+
+print(isTowElemsHandred(array: [2,23,3,97]))
