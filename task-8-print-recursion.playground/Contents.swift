8fc2e7c2a300336104277a02e95cb8250af269ff
JennyShalai <jenny.shalai@gmail.com>
2016-12-20 18:32:32 -0500

    add print func with recursion using built-in popFirst func

--- task-8-print-recursion.playground/Contents.swift
+++ task-8-print-recursion.playground/Contents.swift
@@ -4,24 +4,51 @@ var str = "Hello, playground!"
 
 
 
+
+
+
 ///////////////////   print with recursion   //////////////////////
 
 func printStringWithRecursion(str: String) {
     
     if str.characters.count > 0 {
-        print("! \(str.characters.first!)")
+        print(str.characters.first!)
+    } else {
+        return
     }
     
-    let stringWithoutPrintedChar = str[str.characters.index(after: str.characters.startIndex)..<str.endIndex]
+    // substring, range of chars from 2d to last
+    let stringWithoutPrintedChar = str[str.characters.index(after: str.characters.startIndex)..<str.characters.endIndex]
     
-    if stringWithoutPrintedChar.characters.count > 0 {
-        printStringWithRecursion(str: stringWithoutPrintedChar)
-    }
+    printStringWithRecursion(str: stringWithoutPrintedChar)
+    
     
 }
 
+//printStringWithRecursion(str: str)
 
-printStringWithRecursion(str: str)
+
+func printStrWithRecursion(str: String) {
+    
+    var string = str
+    
+    if str.characters.count > 0 {
+        print(string.characters.popFirst()!)
+        printStrWithRecursion(str: string)
+    } else {
+        return
+    }
+}
+
+//printStrWithRecursion(str: str)
+
+
+
+
+
+
+
+
 
 ///////////////////   print with loop   ///////////////////////////
 
@@ -34,6 +61,27 @@ func printString(str: String) {
 }
 
 //printString(str: str)
+
+
+func printStr(str: String) {
+    
+    var string = str
+    
+    for _ in 0..<str.characters.count {
+        print(string.characters.popFirst()!)
+    }
+}
+
+//printStr(str: str)
+
+
+
+
+
+
+
+
+
 
 //////////////////     print second word    ///////////////////////
 
