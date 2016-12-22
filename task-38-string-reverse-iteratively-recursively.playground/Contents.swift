7f6cfe8070f4a1a528045a1147d87cad023de445
JennyShalai <jenny.shalai@gmail.com>
2016-12-22 09:40:17 -0500

    reverse stringrecursively

--- task-38-string-reverse-iteratively-recursively.playground/Contents.swift
+++ task-38-string-reverse-iteratively-recursively.playground/Contents.swift
@@ -2,14 +2,37 @@
 
 var str = "Hello, playground"
 
+
+
+// Reverse a string iteratively
 func reverseIteratively(str: String) -> String {
-    var input = str
-    var output = ""
-    for i in 0..<input.characters.count {
-        let currentChar = input[input.index(input.startIndex, offsetBy: i)]
-        output = "\(currentChar)\(output)"
+    
+    var result = ""
+    
+    for i in 0..<str.characters.count {
+        let currentChar = str[str.index(str.startIndex, offsetBy: i)]
+        result = "\(currentChar)\(result)"
     }
-    return output
+    
+    return result
+}
+
+
+
+
+// Reverse a string recursively
+func reverseRecursively(str: String) -> String {
+    
+    var result = ""
+    
+    if str.characters.count > 0 {
+        let firstChar = str.characters.first!
+        let strWithoutFirstChar = str[str.characters.index(after: str.characters.startIndex)..<str.characters.endIndex]
+        result = "\(reverseRecursively(str: strWithoutFirstChar))\(firstChar)"
+    }
+    
+    return result
 }
 
 print(reverseIteratively(str: str)) // dnuorgyalp ,olleH
+print(reverseRecursively(str: str)) // dnuorgyalp ,olleH
