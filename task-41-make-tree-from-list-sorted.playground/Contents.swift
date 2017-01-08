c009e148ed4a6d8c081037fc2f0043592174bf1d
JennyShalai <jenny.shalai@gmail.com>
2017-01-08 14:08:07 -0500

    add addNode function

--- task-41-make-tree-from-list-sorted.playground/Contents.swift
+++ task-41-make-tree-from-list-sorted.playground/Contents.swift
@@ -2,19 +2,63 @@
 // Construct a Balanced Binary Search Tree which has same data members as the given Linked List
 
 
-class listNode {
+class ListNode {
     var value: Int
-    var next: listNode?
+    var next: ListNode?
     init(value: Int) {
         self.value = value
     }
 }
 
-class treeNode {
+class TreeNode {
     var value: Int
-    var left: treeNode?
-    var right: treeNode?
+    var left: TreeNode?
+    var right: TreeNode?
+    var parent: TreeNode?
     init(value: Int) {
         self.value = value
     }
+}
+
+class Tree {
+    var head: TreeNode
+    
+    init(head: TreeNode) {
+        self.head = head
+    }
+    
+    func addNode(node: TreeNode?) {
+        
+        var value = 0
+        if node != nil {
+            value = node!.value
+        } else {
+            return
+        }
+        
+        var h = self.head
+        
+        while true {
+            if value <= h.value && h.left == nil {
+                h.left = node
+                node!.parent = h
+                break
+            }
+            
+            if value > h.value && h.right == nil {
+                h.right = node
+                node!.parent = h
+                break
+            }
+            
+            if value <= h.value && h.left != nil {
+                h = h.left!
+            }
+            
+            if value > h.value && h.right != nil {
+                h = h.right!
+            }
+        }
+    }
+    
 }
