f40d51896bdc47afa8d588fd3b5f1944e128be5a
JennyShalai <jenny.shalai@gmail.com>
2017-01-09 09:13:27 -0500

    add print tree func

--- task-42-make-tree-from array-sorted.playground/Contents.swift
+++ task-42-make-tree-from array-sorted.playground/Contents.swift
@@ -12,7 +12,7 @@ class Node {
 
 
 
-
+// make BST from sorted array
 func treeFromArray(array: [Int]) -> Node? {
     
     if array.count == 0 {
@@ -26,3 +26,30 @@ func treeFromArray(array: [Int]) -> Node? {
     return tree
 }
 
+
+
+// print out tree
+func printTree(node: Node?, offset: Int = 0) {
+    if let nodeValue = node?.value {
+        if offset == 0 {
+            print("\(nodeValue)")
+        } else {
+            print(String.init(repeating: "│ ", count: offset - 1) + "└ " + "\(nodeValue)")
+        }
+        printTree(node: node?.left, offset: offset+1)
+        printTree(node: node?.right, offset: offset+1)
+    }
+}
+
+let tree = treeFromArray(array: [1,2,3,4,5,6,7,8,9])
+printTree(node: tree)
+
+//    5
+//    └ 3
+//    │ └ 2
+//    │ │ └ 1
+//    │ └ 4
+//    └ 8
+//      └ 7
+//      │ └ 6
+//      └ 9
